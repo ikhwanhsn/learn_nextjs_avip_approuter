ad74a378a2bcf2ed21fb185275b84e1912d7cb84
ikhwanhsn <ikhwanulhusna111@gmail.com>
2024-02-20 06:09:43 +0700

    chore: add firebase database

---
+++ src/libs/firebase/init.ts
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA1hMfxhXiBtXaY8gtXdsNq059EJmN6C-w",
+  authDomain: "learn-next-app-33372.firebaseapp.com",
+  projectId: "learn-next-app-33372",
+  storageBucket: "learn-next-app-33372.appspot.com",
+  messagingSenderId: "1077007808317",
+  appId: "1:1077007808317:web:b75c9a8d9d99d197603754",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
